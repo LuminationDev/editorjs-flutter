5b0b9c3843bf71941efec1c2147a519bf844c35f
jlundlumination <66352666+jlundlumination@users.noreply.github.com>
2023-06-23 12:21:06 +0930

    Update orange_button.dart

--- lib/OverlayUIComponents/orange_button.dart
+++ lib/OverlayUIComponents/orange_button.dart
@@ -56,7 +56,7 @@ class EditorJSOrangeButton extends StatelessWidget {
           ),
           Text(
             text,
-            style: AppFonts.Heading3,
+            style: AppFonts.Body,
           ),
         ],
       ),
